434ea4b5aa0cbd7337048952b00dc46c0c53d16e
Tong Sun <suntong@cpan.org>
2020-06-06 09:40:48 -0400

    - [*] sample2 to serve directly from already-compressed files

--- app/static1.go
+++ app/static1.go
@@ -10,11 +10,15 @@ func main() {
 	// => http://localhost:3000/css/style.css
 
 	app.Static("/sample0", "../web/sample0")
-	app.Static("/sample2", "../web/sample2")
 
 	app.Static("/sample1", "../web/sample1")
 	// => http://localhost:3000/sample1/js/script.js
 	// => http://localhost:3000/sample1/css/style.css
+
+	// to serve directly from already-compressed files with the .fiber.gz suffix
+	app.Static("/sample2", "../web/sample2", fiber.Static{
+		Compress: true,
+	})
 
 	app.Listen(3000)
 }
