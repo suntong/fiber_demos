8d6b73b662e850b929419196b9a28420b89c3cc7
Tong Sun <suntong@cpan.org>
2020-06-13 08:57:39 -0400

    - [+] make sample2/ works; add static-gz

---
+++ app/static-gz.go
@@ -0,0 +1,25 @@
+package main
+
+import "github.com/gofiber/fiber"
+
+func main() {
+	app := fiber.New()
+
+	//app.Static("/", "./public")
+	// => http://localhost:3000/js/script.js
+	// => http://localhost:3000/css/style.css
+
+	app.Static("/sample0", "../web/sample0")
+
+	app.Static("/sample1", "../web/sample1")
+	// => http://localhost:3000/sample1/js/script.js
+	// => http://localhost:3000/sample1/css/style.css
+
+	// to serve directly from already-compressed files with the .gz suffix
+	app.Settings.CompressedFileSuffix = ".gz" // default: ".fiber.gz"
+	app.Static("/sample-gz", "../web/sample-gz", fiber.Static{
+		Compress: true,
+	})
+
+	app.Listen(3000)
+}
