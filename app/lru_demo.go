79851a404971421aa2a0f3c1af3e96d4095f77c3
Tong Sun <suntong@cpan.org>
2020-09-12 18:23:45 -0400

    - [!] lru_demo "absurd" behavior fixed

--- app/lru_demo.go
+++ app/lru_demo.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber"
+	"github.com/gofiber/utils"
 	"github.com/hashicorp/golang-lru/simplelru"
 )
 
@@ -23,10 +24,35 @@ func main() {
 
 	// http://localhost:3000/q?p=something
 	app.Get("/q", func(c *fiber.Ctx) {
-		l.Add(c.Query("p"), nil)
+		p := utils.ImmutableString(c.Query("p"))
+		l.Add(p, nil)
 		r := fmt.Sprintln(l.Keys())
 		c.Send(r)
 	})
 
 	app.Listen(3000)
 }
+
+/*
+
+https://github.com/gofiber/fiber/issues/763
+
+$ curl http://localhost:3000/q?p=AAAAAAAAAAAAA
+[AAAAAAAAAAAAA]
+
+$ curl http://localhost:3000/q?p=BBBBBBBBBBBBB
+[AAAAAAAAAAAAA BBBBBBBBBBBBB]
+
+$ curl http://localhost:3000/q?p=CCCCCCCCCCCCC
+[AAAAAAAAAAAAA BBBBBBBBBBBBB CCCCCCCCCCCCC]
+
+$ curl http://localhost:3000/q?p=DDDDDDDDDDDDD
+[AAAAAAAAAAAAA BBBBBBBBBBBBB CCCCCCCCCCCCC DDDDDDDDDDDDD]
+
+$ curl http://localhost:3000/q?p=EEEEEEEEEEEEE
+[AAAAAAAAAAAAA BBBBBBBBBBBBB CCCCCCCCCCCCC DDDDDDDDDDDDD EEEEEEEEEEEEE]
+
+$ curl http://localhost:3000/q?p=CCCCCCCCCCCCC
+[AAAAAAAAAAAAA BBBBBBBBBBBBB DDDDDDDDDDDDD EEEEEEEEEEEEE CCCCCCCCCCCCC]
+
+*/
