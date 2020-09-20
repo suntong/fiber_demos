b64e36579d1595940a071ce270108d950cc8c2e7
Tong Sun <suntong@cpan.org>
2020-09-20 10:01:44 -0400

    - [+] template-html/ add testing

---
+++ template-html/main_test.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/gofiber/template/html"
+)
+
+func trim(str string) string {
+	trimmed := strings.TrimSpace(regexp.MustCompile(`\s+`).ReplaceAllString(str, " "))
+	trimmed = strings.Replace(trimmed, " <", "<", -1)
+	trimmed = strings.Replace(trimmed, "> ", ">", -1)
+	return trimmed
+}
+
+func Test_Render(t *testing.T) {
+	engine := html.New("./views", ".html")
+	engine.AddFunc("isAdmin", func(user string) bool {
+		return user == "admin"
+	})
+	if err := engine.Load(); err != nil {
+		t.Fatalf("load: %v\n", err)
+	}
+
+	// Partials
+	var buf bytes.Buffer
+	engine.Render(&buf, "index", map[string]interface{}{
+		"Title": "Hello, World!",
+	})
+	expect := `<h2>Header</h2><h1>Hello, World!</h1><h2>Footer</h2>`
+	result := trim(buf.String())
+	if expect != result {
+		t.Fatalf("Expected:\n%s\nResult:\n%s\n", expect, result)
+	}
+	// Single
+	buf.Reset()
+	engine.Render(&buf, "errors/404", map[string]interface{}{
+		"Error": "404 Not Found!",
+	})
+	expect = `<h1>404 Not Found!</h1>`
+	result = trim(buf.String())
+	if expect != result {
+		t.Fatalf("Expected:\n%s\nResult:\n%s\n", expect, result)
+	}
+}
+
+func Test_AddFunc(t *testing.T) {
+	engine := html.New("./views", ".html")
+	engine.AddFunc("isAdmin", func(user string) bool {
+		return user == "admin"
+	})
+	if err := engine.Load(); err != nil {
+		t.Fatalf("load: %v\n", err)
+	}
+
+	// Func is admin
+	var buf bytes.Buffer
+	engine.Render(&buf, "admin", map[string]interface{}{
+		"User": "admin",
+	})
+	expect := `<h1>Hello, Admin!</h1>`
+	result := trim(buf.String())
+	if expect != result {
+		t.Fatalf("Expected:\n%s\nResult:\n%s\n", expect, result)
+	}
+
+	// Func is not admin
+	buf.Reset()
+	engine.Render(&buf, "admin", map[string]interface{}{
+		"User": "john",
+	})
+	expect = `<h1>Access denied!</h1>`
+	result = trim(buf.String())
+	if expect != result {
+		t.Fatalf("Expected:\n%s\nResult:\n%s\n", expect, result)
+	}
+}
+
+func Test_Layout(t *testing.T) {
+	engine := html.New("./views", ".html")
+
+	engine.AddFunc("isAdmin", func(user string) bool {
+		return user == "admin"
+	})
+	if err := engine.Load(); err != nil {
+		t.Fatalf("load: %v\n", err)
+	}
+
+	var buf bytes.Buffer
+	engine.Render(&buf, "index", map[string]interface{}{
+		"Title": "Hello, World!",
+	}, "layouts/main")
+	expect := `<!DOCTYPE html><html><head><title>Hello, World!</title></head><body><h2>Header</h2><h1>Hello, World!</h1><h2>Footer</h2></body></html>`
+	result := trim(buf.String())
+	if expect != result {
+		t.Fatalf("Expected:\n%s\nResult:\n%s\n", expect, result)
+	}
+}
+
+func Test_FileSystem(t *testing.T) {
+	engine := html.NewFileSystem(http.Dir("./views"), ".html")
+
+	engine.AddFunc("isAdmin", func(user string) bool {
+		return user == "admin"
+	})
+	if err := engine.Load(); err != nil {
+		t.Fatalf("load: %v\n", err)
+	}
+
+	var buf bytes.Buffer
+	engine.Render(&buf, "index", map[string]interface{}{
+		"Title": "Hello, World!",
+	}, "layouts/main")
+	expect := `<!DOCTYPE html><html><head><title>Hello, World!</title></head><body><h2>Header</h2><h1>Hello, World!</h1><h2>Footer</h2></body></html>`
+	result := trim(buf.String())
+	if expect != result {
+		t.Fatalf("Expected:\n%s\nResult:\n%s\n", expect, result)
+	}
+}
+
+func Test_Reload(t *testing.T) {
+	engine := html.NewFileSystem(http.Dir("./views"), ".html")
+	engine.Reload(true) // Optional. Default: false
+
+	engine.AddFunc("isAdmin", func(user string) bool {
+		return user == "admin"
+	})
+	if err := engine.Load(); err != nil {
+		t.Fatalf("load: %v\n", err)
+	}
+
+	if err := ioutil.WriteFile("./views/reload.html", []byte("after reload\n"), 0644); err != nil {
+		t.Fatalf("write file: %v\n", err)
+	}
+	defer func() {
+		if err := ioutil.WriteFile("./views/reload.html", []byte("before reload\n"), 0644); err != nil {
+			t.Fatalf("write file: %v\n", err)
+		}
+	}()
+
+	engine.Load()
+
+	var buf bytes.Buffer
+	engine.Render(&buf, "reload", nil)
+	expect := "after reload"
+	result := trim(buf.String())
+	if expect != result {
+		t.Fatalf("Expected:\n%s\nResult:\n%s\n", expect, result)
+	}
+}
